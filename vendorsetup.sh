7c4606c76215928097f30db21c8075f7658dc213
Thiago <diasthiago@protonmail.com>
2025-01-27 14:49:17 -0300

    clone proton-clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,8 @@
 deviceDir=$(gettop)/device/motorola/odessa
 
+# Clone Proton
+git clone https://gitlab.com/LeCmnGend/proton-clang -b clang-18 --depth=1 prebuilts/clang/host/linux-x86/clang-proton
+
 # Permission
 chmod +x ${deviceDir}/applyPatches.sh
 
