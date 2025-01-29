45d14a42bcc509e0edf3298681613c77a2db4e33
Thiago <diasthiago@protonmail.com>
2025-01-29 08:33:21 -0300

    bringup more patches

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,15 @@
 deviceDir=$(gettop)/device/motorola/odessa
 
+# Proton AOSP Removes 
+rm -rf external/zlib
+rm -rf external/zlib-ng
+rm -rf external/jemalloc_new
+
+# Clones ProtonAOSP
+git clone https://github.com/ProtonAOSP/android_external_zlib-ng.git -b rvc external/zlib-ng
+
+git clone https://github.com/ProtonAOSP/android_external_jemalloc_new.git -b rvc external/jemalloc_new
+
 # Clone Proton
 git clone https://gitlab.com/LeCmnGend/proton-clang -b clang-18 --depth=1 prebuilts/clang/host/linux-x86/clang-proton
 
