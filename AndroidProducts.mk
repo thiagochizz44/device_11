11d2cdbdf705f61c17b2851dabb62286e74e560b
Thiago <diasthiago@protonmail.com>
2025-04-12 15:02:08 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/dot_odessa.mk
+    $(LOCAL_DIR)/lineage_odessa.mk
 
 COMMON_LUNCH_CHOICES := \
-    dot_odessa-eng \
-    dot_odessa-user \
-    dot_odessa-userdebug
+    lineage_odessa-eng \
+    lineage_odessa-user \
+    lineage_odessa-userdebug
